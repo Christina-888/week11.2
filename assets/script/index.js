8e8ca11b1b218cf08d2db64114f89b92657b3a57
Christina-888 <christinabykadorova@gmail.com>
2025-01-26 15:55:49 +0300

    block button

--- assets/script/index.js
+++ assets/script/index.js
@@ -9,15 +9,22 @@ const sumThree = +productThree.innerHTML;
 const sumFour = +productFour.innerHTML;
 
 const totalPrice = document.getElementById('total');
+const priceWithoutDiscount = document.getElementById('first-sum');
 
 let totalSum = sumOne + sumTwo + sumThree + sumFour;
+const countWithoutDiscount = () => {
+  priceWithoutDiscount.innerHTML = `ОБЩАЯ СУММА: ${totalSum.toLocaleString('ru-RU')} галеонов.`
+};
+countWithoutDiscount();
+
+const tapButton = document.querySelector('.price-container__btn');
 
 const countWithDiscount = () => {
   const calculatePrice = totalSum * 0.8;
   totalPrice.innerHTML = `ИТОГО: ${calculatePrice.toLocaleString('ru-RU')} галеонов.`
+  tapButton.disabled = true;
 };
 
-const tapButton = document.querySelector('.price-container__btn');
 tapButton.addEventListener('click', countWithDiscount);
 
 
